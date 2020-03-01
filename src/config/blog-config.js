878f232e183fe23998174c3cc01530e8b76d950c
Ahmed Zetao Yang <zetao.yang@hotmail.com>
2020-03-01 18:04:37 +0800

    feat: add disqus comment

--- src/config/blog-config.js
+++ src/config/blog-config.js
@@ -2,10 +2,12 @@ module.exports = {
   url: 'https://zty.js.org',
   title: 'Zetao\'s Blog',
   description: 'Chase the excellence, success will follow you.',
+  siteName: 'Zetao\'s site',
   copyright: '© 2016 – 2020 Zetao\'s site',
   // postsPerPage: 7,
   googleAnalyticsId: 'UA-85037972-4',
   author: {
-    name: 'Zetao Yang'
+    name: 'Zetao Yang',
+    disqus: 'zty' // disqus shortname
   },
 };
